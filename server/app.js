b29d05da6c4effa2d51c082759f25258a60e670f
subu <subramanya4shenoy@gmail.com>
2021-11-20 19:44:34 +0530

    dotenv and mongoose setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
+
+dotenv.config();
 
 app.use((req, res, next) => {
     console.log("server express started");
 })
 
-app.listen(8080);
+mongoose
+.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.s8zg4.mongodb.net/${process.env.MONGO_DB_DEFAULTDB}?retryWrites=true&w=majority`)
+.then(result => {
+    app.listen(process.env.PORT);
+    console.log("connected to mongoDB")
+})
+.catch( err => {
+    console.log("error Mongoose", err);
+})
