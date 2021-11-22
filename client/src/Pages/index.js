256e713bd38c694849bf5adf1e99841b83e75076
Subramanya Shenoy <subramanya4shenoy@gmail.com>
2021-11-22 23:43:21 +0530

    style UI kit

---
+++ client/src/Pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThemeConfig from './theme';
+
+const Landing = () => {
+    return (
+        <div>
+            Welcome
+        </div>
+    )
+}
+
+export default Landing;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ThemeConfig from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeConfig>
+
+    </ThemeConfig>
   </React.StrictMode>,
   document.getElementById('root')
 );
