13ce75234b3cde36a04690263843dde45cd97b83
subu <subramanya4shenoy@gmail.com>
2021-11-27 21:32:49 +0530

    connected mongo DB and auth

--- server/Shemas/Resolver.js
+++ server/Shemas/Resolver.js
@@ -1,12 +1,13 @@
+const User = require('../Models/user');
 
 const resolvers = {
     Query: {
         getFeedbacks(parents, args) {
             let { type } = args;
-            if(type === 'Given') {
-             return [{message: "given 1"}, {message: "given 2 "}, {message: "given 1"}, {message: "given 2 "}];
+            if (type === 'Given') {
+                return [{ message: "given 1" }, { message: "given 2 " }, { message: "given 1" }, { message: "given 2 " }];
             } else {
-                return [{message: "received 1"}, {message: "receiver 2 "}];
+                return [{ message: "received 1" }, { message: "receiver 2 " }];
             }
         },
 
@@ -19,7 +20,7 @@ const resolvers = {
     Mutation: {
         postFeedback(parent, args) {
             const { message } = args;
-            return {message: `${message}`};
+            return { message: `${message}` };
         },
 
         deleteFeedback(parent, args) {
@@ -28,7 +29,23 @@ const resolvers = {
         },
 
         AuthenticateFacebookUser(parent, args) {
-            return ({name: "subu", profilePic: "https://5.imimg.com/data5/PD/CS/MY-28139693/fresh-hapus-mango-500x500.jpg", accessToken: "awdwwwwq_Token"})
+            console.log(args);
+            const { userID, accessToken, name, graphDomain } = args;
+            return User.findOne({ userID: userID })
+                .then((userDoc) => {
+                    console.log(userDoc);
+                    if (userDoc) {
+                        console.log("user Exists");
+                        return ({ name: "subu", profilePic: "https://5.imimg.com/data5/PD/CS/MY-28139693/fresh-hapus-mango-500x500.jpg", accessToken: "awdwwwwq_Token" });
+                    } else {
+                        const tempUser = new User({ userID: userID, name: name, source: graphDomain});
+                        return tempUser.save()
+                            .then(result => {
+                                console.log("success!!!");
+                                return ({ name: "subu", profilePic: "https://5.imimg.com/data5/PD/CS/MY-28139693/fresh-hapus-mango-500x500.jpg", accessToken: "awdwwwwq_Token" });
+                            }).catch((err) => { console.log("error"); })
+                    }
+                }).catch(err => { console.log(err); })
         }
     }
 }
