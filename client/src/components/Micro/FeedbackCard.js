00e6b0257fd0478672b769633f8fed4a5d24728d
AKSHAY BHAT <akshaybhat301094@gmail.com>
2021-11-28 22:14:23 +0530

    implemented copy to clipboard functionality and fixed console issues (#12)

--- client/src/components/Micro/FeedbackCard.js
+++ client/src/components/Micro/FeedbackCard.js
@@ -1,38 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import cactus from '../../assets/images/cactus.svg';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import FeedbackShareCard from './FeedbackShareCard';
 
-const FeedbackCard = ({message}) => {
+const FeedbackCard = ({ message }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    return (
-        <>
-        <div className="border border-primarylighter flex rounded-lg w-full my-2 bg-primarycontrastText p-4 cursor-pointer hover:bg-primarylighter"
-            onClick={(e) => {handleOpen()}}>
-            <div className="h-10 w-10 rounded-full bg-primarylighter self-center mr-4 flex-shrink-0"> 
-                <img className="mx-auto h-full" src={cactus} alt="U"/>
-            </div>
-            <div className="futuraMedium text-textCommon opacity-75 text-sm self-center">
-              {message}
-            </div>
-        </div>
-        <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+  return (
+    <>
+      <div
+        className='border border-primarylighter flex rounded-lg w-full my-2 bg-primarycontrastText p-4 cursor-pointer hover:bg-primarylighter'
+        onClick={(e) => {
+          handleOpen();
+        }}
       >
-        <FeedbackShareCard feedback={message}/>
-      </Modal>
-    </div>
-        </>
-    )
-}
+        <div className='h-10 w-10 rounded-full bg-primarylighter self-center mr-4 flex-shrink-0'>
+          <img className='mx-auto h-full' src={cactus} alt='U' />
+        </div>
+        <div className='futuraMedium text-textCommon opacity-75 text-sm self-center'>
+          {message}
+        </div>
+      </div>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+        >
+          <>
+            <FeedbackShareCard feedback={message} />
+          </>
+        </Modal>
+      </div>
+    </>
+  );
+};
 
-export default FeedbackCard
+export default FeedbackCard;
