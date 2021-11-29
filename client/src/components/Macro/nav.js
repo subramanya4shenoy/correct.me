e8944ff2e755d0ce2b3cf9dfdb8264ccc02a5cca
AKSHAY BHAT <akshaybhat301094@gmail.com>
2021-11-29 21:11:22 +0530

    Logout functionality and conditional rendering of button (#17)
    * implemented copy to clipboard functionality and fixed console issues
    
    * added copy animation
    
    * added changes for copy styling and toast implementation
    
    * added logout

--- client/src/components/Macro/nav.js
+++ client/src/components/Macro/nav.js
@@ -1,15 +1,45 @@
-import React from 'react'
-import Logo from '../Micro/Logo'
+import React, { useState } from 'react';
+import Logo from '../Micro/Logo';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
-    return (
-        <div className="flex justify-between w-screen px-20">
-            <div className="self-center"><Logo/></div>
-            <Button className="futuraMedium self-center" variant="text" component={Link} to="/login">Login</Button>
-        </div>
-    )
-}
+  const [loggedIn, setLoggedIn] = useState(() => {
+    // getting stored value from localstorage to check if user is authenticated
+    // to be replaced later with usecontext
+    const saved = localStorage.getItem('user');
+    const initialValue = JSON.parse(saved);
+    return initialValue || '';
+  });
 
-export default Nav
+  return (
+    <div className='flex justify-between w-screen px-20'>
+      {console.log(loggedIn)}
+      <div className='self-center'>
+        <Logo />
+      </div>
+      {!loggedIn ? (
+        <Button
+          className='futuraMedium self-center'
+          variant='text'
+          component={Link}
+          to='/login'
+        >
+          Login
+        </Button>
+      ) : (
+        <Button
+          className='futuraMedium self-center'
+          variant='text'
+          onClick={() => localStorage.clear()}
+          component={Link}
+          to='/'
+        >
+          Logout
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default Nav;
