8bbefe9ebd72dbe12b394e8d650f4fc5c9b763ee
subu <subramanya4shenoy@gmail.com>
2021-12-02 01:13:46 +0530

    refresh button and feedbacklist cleaning up

--- client/src/components/Feedback.js
+++ client/src/components/Feedback.js
@@ -1,31 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import FeedBackList from './Macro/FeedBackList';
-import Menu from './Macro/Menu';
 import Nav from './Macro/nav';
 import ShareLink from './Macro/ShareLink';
 
 const Feedback = ({ feedbackId }) => {
-  const [selectedMenu, setselectedMenu] = useState('Recieved');
 
   return (
     <>
       <Nav />
       <div className='futuraMedium text-textCommon text-center py-4 w-full text-4xl'>
-        {' '}
         Hi Subu !
       </div>
       <ShareLink />
       <div className='flex justify-center'>
         <div className='w-3/12'>
-          <Menu
-            selected={selectedMenu}
-            onMenuChange={(e) => {
-              setselectedMenu(e);
-            }}
-          />
         </div>
         <div className='w-9/12'>
-          <FeedBackList selectedMenu={selectedMenu} />
+          <FeedBackList />
         </div>
       </div>
       {feedbackId && <>popover</>}
--- client/src/components/Macro/FeedBackList.js
+++ client/src/components/Macro/FeedBackList.js
@@ -3,25 +3,30 @@ import Chip from '@mui/material/Chip';
 import FeedbackCard from '../Micro/FeedbackCard';
 import { GET_FEEDBACKS } from '../../Resolvers/Feedbacks';
 import { useLazyQuery } from '@apollo/client';
+import Button from '@mui/material/Button';
 
-const FeedBackList = ({selectedMenu}) => {
+const FeedBackList = () => {
 
     const [getFeedbacks, { loading, error, data }] = useLazyQuery(GET_FEEDBACKS);
 
     useEffect(() => {
         console.log("menu changed");
         getFeedbacks()
-    }, [selectedMenu])
+    }, [])
 
     return (
         <div className="w-2/3">
             <div className="flex w-full mx-auto justify-between">
-                <div className="text-textCommon futuraMedium text-sm self-center opacity-75">Feedbacks {selectedMenu}</div>
-                <div className="self-center">  <Chip label="4 New" color="primary" /></div>
+                <div className="text-textCommon futuraMedium text-sm self-center opacity-75">Feedbacks Received</div>
+                <div className="self-center">
+                    <Button variant="text" >
+                        refresh
+                    </Button>
+                </div>
             </div>
 
             <div className="mt-4 rounded-t-lg p-4 h-auto max-h-screen overflow-auto pb-72">
-                {(data) &&
+                {(!loading && data) &&
                     (data.getFeedbacks.map((feedback, index) => {
                         return <FeedbackCard message={feedback.message} key={index + "_messages"} />
                     }))
