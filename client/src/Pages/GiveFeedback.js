deaf2986505d7246b7918a4122ec2e90f5bb7dde
subu <subramanya4shenoy@gmail.com>
2021-12-03 00:39:50 +0530

    zero state and success work

--- client/src/Pages/GiveFeedback.js
+++ client/src/Pages/GiveFeedback.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router';
 import GiveFeedbackCard from '../components/Micro/GiveFeedbackCard'
 import { useQuery } from '@apollo/client';
 import { GET_USER_INFO } from '../Resolvers/Me';
+import FacebookAuth from '../components/Micro/FacebookAuth';
 
 const GiveFeedback = () => {
     const { feedbackId } = useParams();
+    const [done, setdone] = useState(false);
 
     const { loading, data } = useQuery(GET_USER_INFO, { variables: { id: feedbackId.toString() } });
 
@@ -13,11 +15,21 @@ const GiveFeedback = () => {
         <div>
             {(!loading && data) &&
                 (<>
-                <div className="text-center text-textCommon text-2xl mt-20 futuraMedium">
+                {(!done) && (<div className="text-center text-textCommon text-2xl mt-20 futuraMedium">
                     Give anonymous feedback to
                     <span className="capitalize futuraBold text-primarydark"> {data.getUserWithId} </span>
-                </div>
-                <GiveFeedbackCard feedbackUserId={feedbackId}/>
+                </div>)}
+                {(!done) ? 
+                (<GiveFeedbackCard feedbackUserId={feedbackId} setdone={setdone}/>) :
+                (<div className="text-center my-4">
+                    <div className="text-2xl my-24 text-primarymain futuraBold"> Your feedback has been sent!</div>
+                    <div className="px-10">
+                        <FacebookAuth/>
+                       <div className="text-sm text-textCommon opacity-75 futuraMedium py-10 w-1/2 mx-auto">
+                           If you are new or already has a login try to signIn and get your feedback from your friends.
+                        </div> 
+                    </div>
+                </div>)}
             </>)
             }
         </div>
